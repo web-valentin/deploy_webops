6d8dc4ccc20b978acdbcc28d27783466d4a26085
web-valentin <valentin.kiefl@kiefl.de>
2021-05-26 12:38:06 +0200

    fix production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "vm-lime.multimediatechnology.at", user: "deploy", roles: %w{app db web}, port: 5412
+server "vm-lime.multimediatechnology.at", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -41,11 +41,12 @@ server "vm-lime.multimediatechnology.at", user: "deploy", roles: %w{app db web},
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+  keys: %w(/home/valentin/.ssh/id_rsa),
+  port: 5412,
+  forward_agent: true
+#  auth_methods: %w(password)
+}
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
